c8ee34ecd74ed50f62e524592492e2597c39c82f
Sibusiso75 <sibusisomatebese75@gmail.com>
2024-04-29 09:03:50 +0200

    css

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,8 +10,8 @@ function Header() {
           <span ><MdEmail/> SibusisoMatebese75@gmail.com</span>
           <p><FaPhone /> +27 631008729</p>
           <span style={{display:"flex", fontWeight:"normal"}}><a href="https://github.com/Sibusiso75" 
-          style={{borderLeft:"none", borderRight:"2px double whitesmoke"}}><FaGithub/></a><a href="https://www.linkedin.com/in/Sibusiso-matebese-8a0600265"
-          style={{borderLeft:"none"}}> <FaLinkedin/></a>
+          style={{borderLeft:"none", borderRight:"2px double whitesmoke"}}><FaGithub style={{color:"white"}}/></a><a href="https://www.linkedin.com/in/Sibusiso-matebese-8a0600265"
+          style={{color:"white",borderLeft:"none"}}> <FaLinkedin/></a>
 </span>
 </header> 
     </div>
